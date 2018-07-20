eb3a0cacecba2b0e4c239550d9d315461ad2f7e5
Gabriel FOURNIER <gabfou95@gmail.com>
2018-07-21 01:51:34 +0200

    fdsf

---
+++ Assets/Scripts/Kaboom.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Kaboom : MonoBehaviour {
+
+	public float poid = 1;
+	[Range(0, 1)] public float resitimpact = 0;
+	public float life = 1000;
+	[HideInInspector] public Rigidbody2D rbody;
+
+	CinemachineVirtualCamera vcam;
+    CinemachineBasicMultiChannelPerlin vcamperlin;
+	public GameObject	invoqueondead;
+	public int			numbertoinvoc = 15;
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody2D>();
+		if (tag == "player")
+		{
+			vcam = Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera as CinemachineVirtualCamera;
+			if (vcam)
+        	    vcamperlin = vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space))
+			rbody.velocity = new Vector2((name == "blanc") ? 20 : -20, 0);
+	}
+
+
+	void die()
+	{
+		for (int i = 0; i < numbertoinvoc; i++)
+		{
+			GameObject.Instantiate(invoqueondead, transform.position, Quaternion.identity);
+		}
+		GameObject.Destroy(this);
+	}
+
+    IEnumerator impactoEffect()
+    {
+        vcamperlin = vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        vcamperlin.m_AmplitudeGain = 0.5f;
+        vcamperlin.m_FrequencyGain = 30;
+        yield return new WaitForSeconds(0.3f);
+        vcamperlin.m_AmplitudeGain = 0;
+    }
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		Debug.Log("fdsf");
+		Kaboom impactant;
+
+		if ((impactant = other.gameObject.GetComponent<Kaboom>()) != null)
+		{
+			Vector2 realvelocity = impactant.rbody.velocity - rbody.velocity;
+			if (this.tag == "Player" && vcam != null)
+				StartCoroutine(impactoEffect());
+			life -= realvelocity.magnitude;
+			if (life < 0)
+				die();
+			Debug.Log("fdsf2" + life);
+			// rbody.velocity += realvelocity;
+		}
+	}
+}
