3f6feffc506467484279269ffa8f30380cf63628
bciss <cissback@hotmail.fr>
2018-07-21 03:34:43 +0200

    better code

---
+++ Assets/Scripts/AggressiveProjectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AggressiveProjectile : MonoBehaviour {
+
+
+	public	GameObject	target;
+	[Space]
+	public	float		prec = 10;
+	public	float		speed = 10;
+
+	private Rigidbody2D	rb;
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+		// transform.rotation = Quaternion.LookRotation(target.transform.position);
+		rb.AddForce(((target.transform.position - transform.position) + new Vector3(Random.Range(-prec, prec), Random.Range(-prec, prec), 0)) * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/Aggressor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aggressor : MonoBehaviour {
+
+	public	AggressiveProjectile	projectilePrefab;
+	public	GameObject				target;
+	[Space]
+	public	float		distance = 10;
+	public	float		frequancy = 5;
+
+	private	AggressiveProjectile	projectile;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.Range(frequancy, 100) >= 99) {
+			projectile = (AggressiveProjectile)Instantiate(projectilePrefab, Random.insideUnitCircle.normalized * distance, Quaternion.identity);
+			projectile.target = target;
+		}
+	}
+}
