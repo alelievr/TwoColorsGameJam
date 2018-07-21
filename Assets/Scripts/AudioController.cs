2904753648b28cc48e2c9e55b0c20c61e56e2cff
alelievr <alelievr@student.42.fr>
2018-07-21 17:51:54 +0200

    Added audio manager, boss zones and game manager

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioController : MonoBehaviour
+{
+	public AudioMixer		mixer;
+
+	[Header("Audio fade settings")]
+	public float			fadeDistance = 40;
+
+	[Header("Boss zones")]
+	public BossZone[]		bosses;
+
+	[Header("Audio sources")]
+	public AudioSource		bossAudioStart;
+	public AudioSource		bossAudioLoop;
+
+	public static AudioController	instance;
+
+	const string			backgroundVolume = "BackgroundVolume";
+
+	private void Awake()
+	{
+		instance = this;
+	}
+
+	public void StopBossMusic()
+	{
+	}
+
+	void Update ()
+	{
+		float fadeFactor = 1;
+		string volumeController = null;
+
+		foreach (var boss in bosses)
+		{
+			if (boss.dead)
+				continue ;
+			
+			float bossDistance = (transform.position - boss.transform.position).magnitude - boss.radius;
+			
+			if (bossDistance < fadeDistance)
+			{
+				fadeFactor = bossDistance / fadeDistance;
+				volumeController = boss.volumeControlName;
+			}
+		}
+
+		mixer.SetFloat(backgroundVolume, fadeFactor);
+	}
+}
---
+++ Assets/Scripts/BossZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class BossZone : MonoBehaviour
+{
+	public string		bossName;
+	public string		volumeControlName;
+	public AudioClip	startClip;
+	public AudioClip	loopClip;
+	public bool			dead;
+
+	CircleCollider2D	m_CircleCollider;
+	CircleCollider2D	circleCollider
+	{
+		get
+		{
+			if (m_CircleCollider == null)
+				m_CircleCollider = GetComponent<CircleCollider2D>();
+			return m_CircleCollider;
+		}
+	}
+
+	public float		radius
+	{
+		get { return circleCollider.radius; }
+	}
+
+}
