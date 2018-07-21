65eb2b6656a73144f0a3fc963620ada4c5133ba1
Jean ROMAGNANI <jromagna@student.42.fr>
2018-07-21 05:35:11 +0200

    le jeu est pret

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -21,20 +21,46 @@ public class AsteroidController : MonoBehaviour {
 
 	private void Update()
 	{
-		dir = Directionator();
+		dir = Directionator().normalized;
 	}
 	// Update is called once per frame
 	void FixedUpdate () {
 		//Debug.Log("hor" + Input.GetAxisRaw("Horizontal")+ "ver" + Input.GetAxisRaw("Vertical"));
-		// Debug.Log(rb.velocity);
+		Debug.Log(rb.velocity.magnitude);
 		rb.AddForce(dir * speed * Time.fixedDeltaTime, ForceMode2D.Force);
-		if (rb.velocity.x > 100 || rb.velocity.y > 100)
+		if (Mathf.Abs(rb.velocity.magnitude) > 100)
 		{
+			
 			Vector2 tmp = new Vector2(rb.velocity.x, rb.velocity.y);
-			tmp.x = Mathf.Clamp(tmp.x, -100, 100);
-			tmp.y = Mathf.Clamp(tmp.y, -100, 100);
+
+			// tmp.x = Mathf.Clamp(tmp.x, -100, 100);
+			tmp = tmp.normalized * 100;
+			// tmp.y = Mathf.Clamp(tmp.y, -100, 100);
 			rb.velocity = tmp;
 		}
 
+	}
+
+	void Grossissement()
+	{
+		Vector3 tmp = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+		tmp = tmp * 1.1f;
+		tmp.z = 1f;
+		transform.localScale = tmp;
+		rb.mass *= 1.1f;
+	}
+
+	/// <summary>
+	/// Sent when an incoming collider makes contact with this object's
+	/// collider (2D physics only).
+	/// </summary>
+	/// <param name="other">The Collision2D data associated with this collision.</param>
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "debrit")
+		{
+			Grossissement();
+			Destroy(other.gameObject);
+		}
 	}
 }
---
+++ Assets/Scripts/DebritController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebritController : MonoBehaviour {
+
+	public GameObject player;
+	Rigidbody2D rb;
+	Vector2 dir;
+	float speed = 5000;	
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		
+	}
+
+	Vector2 Directionator()
+	{
+		Vector2 tmp = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
+		return tmp;
+	}
+
+	 private void Update()
+	{
+		dir = Directionator().normalized;
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.AddForce(dir * speed * Time.fixedDeltaTime, ForceMode2D.Force);	
+		if (Mathf.Abs(rb.velocity.magnitude) > 200)
+		{
+			
+			Vector2 tmp = new Vector2(rb.velocity.x, rb.velocity.y);
+
+			// tmp.x = Mathf.Clamp(tmp.x, -100, 100);
+			tmp = tmp.normalized * 200;
+			// tmp.y = Mathf.Clamp(tmp.y, -100, 100);
+			rb.velocity = tmp;
+		}
+	}
+}
