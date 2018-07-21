30ee97d1385f72ada2228efbaa0d4b0f44632385
Gabriel FOURNIER <gabfou95@gmail.com>
2018-07-21 20:41:42 +0200

    Fdsf

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
 
 public class AsteroidController : MonoBehaviour {
 
@@ -8,11 +11,24 @@ public class AsteroidController : MonoBehaviour {
 	public float maxSpeed = 100;
 	Vector2 dir;
 	Rigidbody2D rb;
+	public AudioClip boostsound;
+
+	float boostmult = 1;
+	public float timeboostins = 0.3f;
+	public float boostcd = 1;
+	CinemachineImpulseSource cis;
+	public CinemachineVirtualCamera vcam;
+	CinemachineFramingTransposer vcamframing;
+
+	public PlayableDirector	director;
 
 	// Use this for initialization
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		cis = GetComponent<CinemachineImpulseSource>();
+		// vcam = Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera as CinemachineVirtualCamera;
+		vcamframing = vcam.GetCinemachineComponent<CinemachineFramingTransposer>();
 	}
 	
 	Vector2 Directionator()
@@ -21,23 +37,53 @@ public class AsteroidController : MonoBehaviour {
 		return tmp;
 	}
 
+	IEnumerator boost()
+    {
+		director.Play();
+		float tmptime = timeboostins;
+		boostcdtmp = boostcd;
+		// cis.GenerateImpulse(rb.velocity);
+		// vcamframing.m_XDamping = 1.5f;
+        // vcamframing.m_YDamping = 1.5f;
+        // vcamframing.m_ZDamping = 1.5f;
+		
+		boostmult = 2;
+		while (tmptime > 0)
+		{
+			tmptime -= Time.deltaTime;
+			// vcamframing.m_LookaheadTime = tmptime / timeboostins * 0.05f;
+			// vcamframing.m_XDamping = tmptime / timeboostins * 1.5f;
+			// vcamframing.m_YDamping = tmptime / timeboostins * 1.5f;
+			// vcamframing.m_ZDamping = tmptime / timeboostins * 1.5f;
+			yield return new WaitForEndOfFrame();
+		}
+		// vcamframing.m_LookaheadTime = 0.05f;
+		// vcamframing.m_XDamping = 0;
+        // vcamframing.m_YDamping = 0;
+        // vcamframing.m_ZDamping = 0;
+		boostmult = 1;
+    }
+
+	float boostcdtmp = -1;
+
 	private void Update()
 	{
+		if (boostcdtmp >= 0)
+			boostcdtmp -= Time.deltaTime;
 		dir = Directionator().normalized;
+		if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(0)
+		|| Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.LeftControl)) && boostcdtmp < 0)
+			StartCoroutine(boost());
 	}
 	// Update is called once per frame
 	void FixedUpdate () {
 		//Debug.Log("hor" + Input.GetAxisRaw("Horizontal")+ "ver" + Input.GetAxisRaw("Vertical"));
 		Debug.Log(rb.velocity.magnitude);
-		rb.AddForce(dir * speed, ForceMode2D.Force);
-		if (Mathf.Abs(rb.velocity.magnitude) > maxSpeed)
+		rb.AddForce(dir * speed * boostmult, ForceMode2D.Force);
+		if (Mathf.Abs(rb.velocity.magnitude) > maxSpeed * boostmult)
 		{
-			
 			Vector2 tmp = new Vector2(rb.velocity.x, rb.velocity.y);
-
-			// tmp.x = Mathf.Clamp(tmp.x, -100, 100);
-			tmp = tmp.normalized * maxSpeed;
-			// tmp.y = Mathf.Clamp(tmp.y, -100, 100);
+			tmp = tmp.normalized * maxSpeed * boostmult;
 			rb.velocity = tmp;
 		}
 
