9e3889cb6f94776ee33019390dbd006ab9f53ed0
Gabriel FOURNIER <gabfou95@gmail.com>
2018-07-22 12:12:58 +0200

    fsdf

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -23,11 +23,13 @@ public class Laser : MonoBehaviour {
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
-		Destroy(gameObject);
+		if (other.tag == "Player")
+			Destroy(gameObject);
 	}
 
 	private void OnCollisionEnter2D(Collision2D other)
 	{
-		Destroy(gameObject);
+		if (other.tag == "Player")
+			Destroy(gameObject);
 	}
 }
