2bf92bb157303f3578129b7c88981627b515f00f
Antoine Lelievre <antoinel@unity3d.com>
2018-08-06 19:26:21 +0200

    Added mobile shader and switched to legacy renderer, faster than lightweight

---
+++ Assets/Scripts/InitializeGraphicSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeGraphicSettings
+{
+	[RuntimeInitializeOnLoadMethod]
+	static void Init()
+	{
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 60;
+	}
+}
