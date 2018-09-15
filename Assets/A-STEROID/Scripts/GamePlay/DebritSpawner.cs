be15994ebc7cd120504b4ead526569fd744a8937
alelievr <alelievr@student.42.fr>
2018-09-16 01:55:32 +0200

    Added new enemy and debris spawners

---
+++ Assets/A-STEROID/Scripts/GamePlay/DebritSpawner.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebritSpawner : MonoBehaviour
+{
+	public int			chunkSize;
+	public int			chunkLoadCount = 1;
+	public int			debrisPerChunk = 1;
+	public float		noiseIntensity = 2;
+	public float		turbulance = 0.1f;
+
+	float				debrisDensity;
+
+	HashSet<Vector2Int>	loadedChunks = new HashSet<Vector2Int>();
+	Vector2Int			oldPlayerChunk;
+
+	private void Start()
+	{
+		debrisDensity = (1f / debrisPerChunk) * chunkSize;
+		UpdateChunks(Vector2Int.zero);
+	}
+
+	void Update ()
+	{
+		Vector2 p = GameManager.instance.playerTransform.position / chunkSize;
+		Vector2Int playerChunk = new Vector2Int((int)p.x, (int)p.y);
+
+		if (playerChunk != oldPlayerChunk)
+		{
+			UpdateChunks(playerChunk);
+		}
+
+		oldPlayerChunk = playerChunk;
+	}
+
+	void UpdateChunks(Vector2Int playerChunkPos)
+	{
+		for (int x = -chunkLoadCount; x <= chunkLoadCount; x++)
+		{
+			for (int y = -chunkLoadCount; y <= chunkLoadCount; y++)
+			{
+				Vector2Int chunkPos = playerChunkPos + new Vector2Int(x, y);
+				if (!loadedChunks.Contains(chunkPos))
+					LoadChunk(chunkPos);
+			}
+		}
+	}
+
+	void LoadChunk(Vector2Int chunkPos)
+	{
+		Vector2 cellSize = new Vector2(chunkSize / debrisPerChunk, chunkSize / debrisPerChunk) / 2f;
+
+		for (float x = 0; x < chunkSize; x += debrisDensity)
+		{
+			for (float y = 0; y < chunkSize; y += debrisDensity)
+			{
+				Vector3 pos = chunkPos * chunkSize + new Vector2(x, y) - new Vector2(chunkSize / 2f, chunkSize / 2f) + cellSize;
+
+				pos.x += (Mathf.PerlinNoise(pos.x * turbulance, pos.y * turbulance) - 0.5f) * debrisPerChunk * noiseIntensity;
+				pos.y += (Mathf.PerlinNoise(pos.x * turbulance + 1000, pos.y * turbulance + 1000) - 0.5f) * debrisPerChunk * noiseIntensity;
+
+				DebrisPool.instance.NewDebris(pos);
+			}
+		}
+		loadedChunks.Add(chunkPos);
+	}
+
+	private void OnDrawGizmos()
+	{
+		foreach (var v in loadedChunks)
+			Gizmos.DrawWireCube((Vector2)v * chunkSize, Vector3.one * chunkSize);
+	}
+}
---
+++ Assets/A-STEROID/Scripts/GamePlay/EnemySpawner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[System.Serializable]
+public class EnemySpawnerInfo
+{
+	public GameObject	prefab;
+	public float		spawnAfterDistance = 0;
+	public float		spawnRatio = 1;
+	public float		spawnRatioOverDistance = 0;
+}
+
+public class EnemySpawner : MonoBehaviour
+{
+	// TODO: pollify this
+	public List< EnemySpawnerInfo >	enemies = new List< EnemySpawnerInfo >();
+
+	[Space, Header("Spawn settings")]
+	public float	spawnDelay = 5;
+	public float	spawnDelayVariation = 2f;
+
+	public float traveledDistance
+	{
+		get { return 0; } // TODO: implement this
+	}
+
+	void Start ()
+	{
+		StartCoroutine(SpawnEnemies());
+	}
+	
+	IEnumerator SpawnEnemies()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(spawnDelay + Random.Range(-spawnDelayVariation / 2f, spawnDelayVariation / 2f));
+			
+			float totalSpawnRatio = enemies.Sum((e) => {
+				if (traveledDistance < e.spawnAfterDistance)
+					return 0;
+				float dist = traveledDistance - e.spawnAfterDistance;
+				return e.spawnRatio + e.spawnRatioOverDistance * dist;
+			});
+
+			float r = Random.Range(0, totalSpawnRatio);
+			float d = 0;
+			foreach (var e in enemies)
+			{
+				if (traveledDistance < e.spawnAfterDistance)
+					continue ;
+				
+				float dist = traveledDistance - e.spawnAfterDistance;
+				d += e.spawnRatio + e.spawnRatioOverDistance * dist;
+				if (r >= d)
+				{
+					SpawnEnemy(e.prefab);
+					break ;
+				}
+			}
+		}
+	}
+
+	void SpawnEnemy(GameObject prefab)
+	{
+		Vector3 pos = (Vector2)GameManager.instance.playerTransform.position + Random.insideUnitCircle.normalized * GameManager.instance.playerSize * 2;
+		GameObject.Instantiate(prefab, pos, Quaternion.identity);
+	}
+}
